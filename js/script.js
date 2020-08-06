9cf69e7397672f297bef2608b10dd987a589d2d9
joseph <giuseppe.falco000@gmail.com>
2020-08-06 17:01:42 +0200

    adjusted

--- js/script.js
+++ js/script.js
@@ -2,25 +2,28 @@
 
 //chiedo il nome
 var name = prompt("Inserisci il tuo nome");
-if ((name == "") || (name == " ")){
-  name = prompt("Ops, hai sbagliato qualcosa! Riprova");
-}
-document.getElementById("name-html").innerHTML = name
+if (!((name == "") || (name == " "))){
+  document.getElementById("name-html").innerHTML = name
 
-//chiedo il sesso
-var sex = prompt("Inserisci il tuo sesso, puoi scegliere tra 'uomo'/'maschio' oppure 'donna'/'femmina'. Tutte le altre scelte non saranno valide.  ");
-if ((sex == "") || (sex == " ")){
-  sex = prompt("Ops, hai sbagliato qualcosa! Riprova");
-} else if (sex == 'uomo' || sex == 'maschio'){
-    document.getElementById("sex-html").innerHTML = sex;
-    sex = document.getElementById("sex-html");
-    sex.style.color = 'blue';
-} else if ( sex == 'donna' || sex == 'femmina'){
-    document.getElementById("sex-html").innerHTML = sex;
-    sex = document.getElementById("sex-html");
-    sex.style.color = 'pink';
+  //chiedo il sesso
+  var sex = prompt("Inserisci il tuo sesso, puoi scegliere tra 'uomo'/'maschio' oppure 'donna'/'femmina'. Tutte le altre scelte non saranno valide.  ");
+  if ((sex == "") || (sex == " ")){
+    sex = prompt("Ops, hai sbagliato qualcosa! Riprova");
+  } else if (sex == 'uomo' || sex == 'maschio'){
+      document.getElementById("sex-html").innerHTML = sex;
+      sex = document.getElementById("sex-html");
+      sex.style.color = 'blue';
+  } else if ( sex == 'donna' || sex == 'femmina'){
+      document.getElementById("sex-html").innerHTML = sex;
+      sex = document.getElementById("sex-html");
+      sex.style.color = 'pink';
+  } else {
+      var error = "Ops, hai sbagliato qualcosa!";
+      document.getElementById("sex-html").innerHTML = error;
+      error = document.getElementById('sex-html');
+      error.style.color = 'red';
+  }
+
 } else {
-    var error = "Ops, hai sbagliato qualcosa!"
-    document.getElementById("sex-html").innerHTML = error;
-    sex.style.color = 'red';
+  name = alert("Ops, hai sbagliato qualcosa! Riprovas");
 }
